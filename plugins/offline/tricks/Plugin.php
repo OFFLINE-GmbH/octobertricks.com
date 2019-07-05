67602a753bb45be4cb3482597fed42c5efeaec78
Tobias Kündig <tobias@offline.swiss>
2019-07-05 07:41:08 +0200

    Optionally extend user model

--- plugins/offline/tricks/Plugin.php
+++ plugins/offline/tricks/Plugin.php
@@ -33,20 +33,22 @@ class Plugin extends PluginBase
 
     public function boot()
     {
-        User::extend(function (User $user) {
-            $user->rules['password']              = 'required:create|between:8,255|confirmed';
-            $user->rules['password_confirmation'] = 'required_with:password|between:8,255';
-            $user->addDynamicMethod('getFallbackAvatarAttribute', function () use ($user) {
-                $id = str_slug($user->username);
+        if (class_exists('RainLab\User\Models\User')) {
+            User::extend(function (User $user) {
+                $user->rules['password']              = 'required:create|between:8,255|confirmed';
+                $user->rules['password_confirmation'] = 'required_with:password|between:8,255';
+                $user->addDynamicMethod('getFallbackAvatarAttribute', function () use ($user) {
+                    $id = str_slug($user->username);
 
-                $url = 'https://api.adorable.io/avatars/%d/%s.png';
+                    $url = 'https://api.adorable.io/avatars/%d/%s.png';
 
-                return [
-                    sprintf($url, 45, $id),
-                    sprintf($url, 90, $id),
-                ];
+                    return [
+                        sprintf($url, 45, $id),
+                        sprintf($url, 90, $id),
+                    ];
+                });
             });
-        });
+        }
 
         Event::listen('pages.menuitem.listTypes', function () {
             return [
