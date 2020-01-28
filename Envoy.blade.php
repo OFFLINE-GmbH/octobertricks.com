80279125f36e6e5bdb8c634a6c988c342db79484
Tobias Kündig <tobias@offline.swiss>
2020-01-28 17:37:23 +0100

    Removed update check

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -23,32 +23,6 @@
     $PHP ./vendor/bin/october install --php=$PHP
     $PHP artisan -v october:up
 
-    ## START UPDATE CHECK
-    LOCK_FILE=".last-update-check"
-    NOW=$(date +%s)
-    LAST_CHECK=$( [ -f $LOCK_FILE ] && cat $LOCK_FILE || echo 0 )
-    SECONDS_SINCE=$(expr $NOW - $LAST_CHECK)
-
-    if [ "$SECONDS_SINCE" -gt "86400" ]; then
-        HOSTNAME=$( hostname )
-        GIT=$( which git )
-
-        $PHP composer.phar self-update
-        $PHP composer.phar update --no-interaction --no-dev --prefer-dist --ignore-platform-reqs
-        $PHP artisan -v october:update
-
-        if [[ -n $(git status -s) ]]; then
-            $GIT add --all .
-            $GIT commit -m "[ci skip] oc-bootstrapper updated October CMS ({{ $project }})"
-            $GIT push origin master
-        fi
-
-        echo $NOW > $LOCK_FILE
-    else
-        echo "Skipping update check (last check was $SECONDS_SINCE seconds ago)"
-    fi
-    ## END UPDATE CHECK
-
     git status -s
 @endtask
 
